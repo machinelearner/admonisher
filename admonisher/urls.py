344de959c821d00c0d3dad523762b2d3d787be6a
Pramod N <npramod05@gmail.com>
2013-06-04 12:08:45 +0530

    Small UI modification; default redirect at /

--- admonisher/urls.py
+++ admonisher/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import RedirectView
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from contacts.views import upload_contacts_csv
@@ -22,6 +23,7 @@ urlpatterns = patterns('',
     #   Application specific URLs    #
     ######                      ######
     #url(r'',)
+    url(r'^$', RedirectView.as_view(url= 'defaulter/excel_upload')),
     url(r'^contacts/csv_upload$', upload_contacts_csv),
     url(r'defaulter/excel_upload',upload_defaulter_excel_sheet),
 
