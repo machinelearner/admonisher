2598041ca4d4672f21cc2da794bcfd03b0164a59
Pramod N <npramod05@gmail.com>
2013-08-23 12:25:48 +0530

    Removing Location filter

--- admonish_defaulter/models.py
+++ admonish_defaulter/models.py
@@ -5,8 +5,6 @@ logger = logging.getLogger("admonish_defaulter")
 class DefaulterAction:
     EXCEL_SHEET_HEADER = ['Employee/Contractor', 'Week Ending Dt', 'Payroll', 'Work in Office', 'Work in Ctry', 'Dept', 'Name', 'Empl ID', 'Project', 'Email ID', 'ILT Member']
     DEFAULTER_HEADER_INDEX = EXCEL_SHEET_HEADER.index('Empl ID')
-    LOCATION_HEADER_INDEX =  EXCEL_SHEET_HEADER.index('Work in Office')
-    LOCATION_FILTER = "bangalore"
 
     @classmethod
     def extract_defaulter_ids_from_excel(self,excel_file):
@@ -16,8 +14,7 @@ class DefaulterAction:
         list_of_defaulter_ids = []
         if worksheet.row_values(1) == self.EXCEL_SHEET_HEADER:
             for row in range(2,worksheet.nrows):
-                if worksheet.row(row)[self.LOCATION_HEADER_INDEX].value.lower() == self.LOCATION_FILTER:
-                    list_of_defaulter_ids.append(worksheet.row(row)[self.DEFAULTER_HEADER_INDEX].value)
+                list_of_defaulter_ids.append(worksheet.row(row)[self.DEFAULTER_HEADER_INDEX].value)
             return list_of_defaulter_ids
         logger.error("The File format has changed; Upload file with following headers")
         logger.error(self.EXCEL_SHEET_HEADER)
